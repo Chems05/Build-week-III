f1cd85300a590688fd7042ec6badfaf317da62a3
Petrov-Teo <168548498+Petrov-Teo@users.noreply.github.com>
2024-07-15 10:49:13 +0200

    Aggiunto Fonts

---
+++ src/assets/components/ProfileInfo.jsx
@@ -0,0 +1,13 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+const ProfileInfo = () => {
+  return (
+    <Container>
+      <Row>
+        <Col></Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default ProfileInfo;
