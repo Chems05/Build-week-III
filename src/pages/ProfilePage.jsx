9db3771bb23e59f01d7ad42d3d530d83cb5ffd9f
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-07-15 15:07:52 +0200

    develop aggiustata

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,9 +1,11 @@
 import Footer from "../components/Footer";
 import Activity from "../components/Activity";
+import MainProfileInfo from "../components/MainProfileInfo";
 
 const ProfilePage = () => {
   return (
     <div>
+      <MainProfileInfo />
       <Footer />
       <Activity />
     </div>
