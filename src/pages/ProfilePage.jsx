bdf499ca8d52d829079080c17903432bf15cfa2f
Gius3pp33 <peppe-2@live.it>
2024-07-15 18:02:45 +0200

    navbar fixed

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -5,19 +5,15 @@ import ProfileInfo from "../components/ProfileInfo";
 import Activity from "../components/Activity";
 import Resource from "../components/Resource";
 import Footer from "../components/Footer";
-import Informazioni from "../components/Informazioni";
-import Competenze from "../components/Competenze";
 
 const ProfilePage = () => {
   return (
-    <div style={{ color: "grey" }}>
+   <>
       <MyNavbar />
       <Container className="mt-4">
         <Row>
           <Col md={8}>
             <MainProfileInfo />
-            <Informazioni />
-            <Competenze />
             <Activity />
             <Resource />
           </Col>
@@ -27,7 +23,8 @@ const ProfilePage = () => {
         </Row>
         <Footer />
       </Container>
-    </div>
+ 
+      </>
   );
 };
 
