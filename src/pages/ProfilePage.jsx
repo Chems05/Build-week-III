5edd2db350076f35dba9e289080a7dd379cd36e6
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-07-15 16:59:27 +0200

    terminato Analytics component

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -3,17 +3,28 @@ import Activity from "../components/Activity";
 import MainProfileInfo from "../components/MainProfileInfo";
 import MyNavbar from "../components/MyNavbar";
 import ProfileInfo from "../components/ProfileInfo";
+import { Col, Container, Row } from "react-bootstrap";
+import Analytics from "../components/Analytics";
 
 const ProfilePage = () => {
   return (
-    <div>
+    <>
       <MyNavbar />
-      <MainProfileInfo />
-      <ProfileInfo />
-      <Footer />
-      <Activity />
-      <Footer />
-    </div>
+      <Container>
+        <Row className="mt-5">
+          <Col md={8}>
+            <MainProfileInfo />
+            <Analytics />
+            <Activity />
+            <Footer />
+          </Col>
+
+          <Col md={4}>
+            <ProfileInfo />
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
