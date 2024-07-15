3c144fd8825264e305e1cf4d442f018117a57097
Gius3pp33 <peppe-2@live.it>
2024-07-15 17:25:50 +0200

    .

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import MyNavbar from "../components/MyNavbar";
 import MainProfileInfo from "../components/MainProfileInfo";
@@ -6,6 +5,8 @@ import ProfileInfo from "../components/ProfileInfo";
 import Activity from "../components/Activity";
 import Resource from "../components/Resource";
 import Footer from "../components/Footer";
+import Informazioni from "../components/Informazioni";
+import Competenze from "../components/Competenze";
 
 const ProfilePage = () => {
   return (
@@ -15,6 +16,8 @@ const ProfilePage = () => {
         <Row>
           <Col md={8}>
             <MainProfileInfo />
+            <Informazioni />
+            <Competenze />
             <Activity />
             <Resource />
           </Col>
