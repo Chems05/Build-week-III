c159c11faf02fbb817939411d992a0d2c068f4db
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-07-15 15:12:48 +0200

    deleted image asset

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,10 +1,12 @@
 import Footer from "../components/Footer";
 import Activity from "../components/Activity";
 import MainProfileInfo from "../components/MainProfileInfo";
+import MyNavbar from "../components/MyNavbar";
 
 const ProfilePage = () => {
   return (
     <div>
+      <MyNavbar />
       <MainProfileInfo />
       <Footer />
       <Activity />
