24f51017577e11ab6108d05f0f3ca55fb10c607b
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-07-17 13:13:01 +0200

    hover ai bottoni delle icone in progress

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,23 @@
-import { Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import AsideLeftHomePage from "../components/AsideLeftHomePage";
+import CreatePost from "../components/CreatePost";
+import MyNavbar from "../components/MyNavbar";
 
 const HomePage = () => {
   return (
-    <Container>
-      <Row>
-        <AsideLeftHomePage />
-      </Row>
-    </Container>
+    <>
+      <MyNavbar />
+      <Container className="mt-4">
+        <Row>
+          <Col md={4}>
+            <AsideLeftHomePage />
+          </Col>
+          <Col md={8}>
+            <CreatePost />
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
