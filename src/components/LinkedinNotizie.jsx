3faf306600497ab48364433cac1c01a624e0bf1e
Petrov-Teo <168548498+Petrov-Teo@users.noreply.github.com>
2024-07-17 22:14:15 +0200

    da completare modali

--- src/components/LinkedinNotizie.jsx
+++ src/components/LinkedinNotizie.jsx
@@ -1,4 +1,4 @@
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Modal, Row } from "react-bootstrap";
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 import { HiInformationCircle } from "react-icons/hi";
 import { useState } from "react";
@@ -18,13 +18,36 @@ const LinkedinNotizie = () => {
   return (
     <Container className="mb-4">
       {showInfo && (
-        <Row className="position-absolute ">
-          <Col>
-            Queste sono le principali notizie e conversazioni del giorno.
-            <span className="link-offset-2 link-underline link-underline-opacity-0">Scopri</span> come vengono
-            selezionate.
-          </Col>
-        </Row>
+        <div className="modal show" style={{ display: "block", position: "initial" }}>
+          <Modal.Dialog>
+            <Modal.Body>
+              <Button className="position-absolut top-150" aria-label="Chiudi" type="button">
+                <li-icon aria-hidden="true" type="cancel-icon" size="small" class="cz-color-0">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    data-supported-dps="16x16"
+                    fill="currentColor"
+                    className="mercado-match cz-color-0"
+                    width="16"
+                    height="16"
+                    focusable="false"
+                  >
+                    <path
+                      d="M14 3.41L9.41 8 14 12.59 12.59 14 8 9.41 3.41 14 2 12.59 6.59 8 2 3.41 3.41 2 8 6.59 12.59 2z"
+                      className="cz-color-0"
+                    ></path>
+                  </svg>
+                </li-icon>
+              </Button>
+              <p>
+                Queste sono le principali notizie e conversazioni del giorno.
+                <span className="link-offset-2 link-underline link-underline-opacity-0">Scopri</span> come vengono
+                selezionate.
+              </p>
+            </Modal.Body>
+          </Modal.Dialog>
+        </div>
       )}
       <Row className="bg-white d-flex justify-content-between border border-dark-subtle rounded-3">
         <Col className="row ps-2 col-8 ms-2">
@@ -62,19 +85,19 @@ const LinkedinNotizie = () => {
             </Col>
             <Col className="row ps-2 col-8 ms-2 hooverElementLinkedinNotizie ">
               <p className="fontPersonale fs-5 m-0 fw-semibold">Ora che l`AI Act è legge</p>
-              <p className="fs-5 text-body-secondary">19 ore fa</p>
+              <p className="fs-5 text-body-secondary">26 ore fa</p>
             </Col>
             <Col className="row ps-2 col-8 ms-2 hooverElementLinkedinNotizie ">
-              <p className="fontPersonale fs-5 m-0 fw-semibold">Due italiane al top nella matematica</p>
-              <p className="fs-5 text-body-secondary">19 ore fa</p>
+              <p className="fontPersonale fs-5 m-0 fw-semibold">Ferragamo crsce nel mondo dei vini</p>
+              <p className="fs-5 text-body-secondary">1 giorno fa</p>
             </Col>
             <Col className="row ps-2 col-8 ms-2 hooverElementLinkedinNotizie ">
-              <p className="fontPersonale fs-5 m-0 fw-semibold">Due italiane al top nella matematica</p>
-              <p className="fs-5 text-body-secondary">19 ore fa</p>
+              <p className="fontPersonale fs-5 m-0 fw-semibold">Alla comunicazione servono processi chiari</p>
+              <p className="fs-5 text-body-secondary">2 gorni fa</p>
             </Col>
           </>
         )}
-        <Col className="col-12">
+        <Col className="col-12 ms-2">
           <Button className="btnVediAltro fs-5 text-dark" onClick={toggleShowFullArticole}>
             {showFullArticole ? (
               <>
@@ -87,6 +110,20 @@ const LinkedinNotizie = () => {
             )}
           </Button>
         </Col>
+        <Row>
+          <Col className="ms-2 mt-4 col-12 ">
+            <h3 className="fs-5 fw-semibold text-body-secondary ">
+              I giochi di oggi <span className=" fs-6 ms-3 bgBadge p-1 "> NOVITÀ </span>
+            </h3>
+          </Col>
+          <Col className="d-flex m-2 gioghi">
+            <img src="https://static.licdn.com/aero-v1/sc/h/25itbd3dpc6ockbgvdhot9qp1" alt="gioco" />
+            <p className="d-inline mt-3 ms-2">
+              <span className="fw-bold ">Queens</span> #78
+              <small className="text-body-secondary d-block">Incorona ogni regione</small>
+            </p>
+          </Col>
+        </Row>
       </Row>
       <Row className="d-flex justify-content-between border border-dark-subtle rounded-3 mt-2">
         <img
@@ -94,13 +131,34 @@ const LinkedinNotizie = () => {
           className="m-0 p-0"
         />
       </Row>
-      <Row className="bg-white d-flex border border-dark-subtle rounded-3 mt-2">
-        <Col xs="12">
-          <p className="fs-6 mt-3 ">
-            <strong>Persone che potresti conoscere</strong>
-            <br />
-            <small className="text-muted">Dalla tua azienda</small>
-          </p>
+      <Row className="text-center mt-3">
+        <Col md="12 mb-2">
+          <span className="footerText">Informazioni</span> <span className="ms-3 footerText">Accessibilità</span>
+        </Col>
+        <Col md="12 mb-2">
+          <span className="footerText">Centro assistenza</span>
+        </Col>
+        <Col md="12 mb-2">
+          <span className="footerText">Privacy e condizioni </span>
+        </Col>
+        <Col md="12 mb-2">
+          <span className="footerText">Opzioni per gli annunci pubblicitari </span>
+        </Col>
+        <Col md="12 mb-2">
+          <span className="footerText">Pubblicià</span> <span className="ms-3 footerText">Servizi alle aziende</span>
+        </Col>
+        <Col md="12 mb-2">
+          <span className="footerText">Scarica l`app Linkedin</span> <span className="ms-3 footerText">Altro</span>
+        </Col>
+      </Row>
+      <Row className="text-center mt-3">
+        <Col>
+          <img
+            src="https://static.licdn.com/aero-v1/sc/h/aahlc8ivbnmk0t3eyz8as5gvr"
+            alt="nomeAzienda"
+            className="imgLinkedin"
+          />
+          <span className="ms-2">LinkedIn Corporation © 2024 </span>
         </Col>
       </Row>
     </Container>
