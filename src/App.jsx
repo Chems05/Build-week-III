6c5c57270c17c71eabe6e3c3a49b40a6295e0d30
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-07-17 09:24:28 +0200

    HomePage creata

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import ProfilePage from "./pages/ProfilePage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<ProfilePage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/" element={<HomePage />} />
         </Routes>
       </BrowserRouter>
     </>
