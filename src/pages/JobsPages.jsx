f2e8a5dda2c57b9c486c84e4adf55d84bf21e7ec
Gius3pp33 <peppe-2@live.it>
2024-07-18 11:18:37 +0200

    parte centrale jobs finita

--- src/pages/JobsPages.jsx
+++ src/pages/JobsPages.jsx
@@ -1,6 +1,7 @@
 import { Col, Container, Row } from "react-bootstrap";
 import MyNavbar from "../components/MyNavbar";
 import LeftJobs from "../components/LeftJobs"
+import MiddleJobs from "../components/MiddleJobs";
 
 const JobsPage = () => {
     return (
@@ -8,10 +9,15 @@ const JobsPage = () => {
         <MyNavbar />
         <Container className="mt-4">
           <Row>
-            <Col md={6}>
+            <Col md={3}>
             <LeftJobs />
             </Col>
-           
+           <Col md={6}
+           >
+            <MiddleJobs />
+           </Col>
+           <Col md={3}>
+           </Col>
           </Row>
         </Container>
       </>
